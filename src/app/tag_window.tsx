c21b14d0b478e2ce6d749cf57ffac4dc8775d9de
Donnalyn <donnalynrrma@gmail.com>
2023-12-14 18:04:03 +0800

    fixed tags overlapping

--- src/app/tag_window.tsx
+++ src/app/tag_window.tsx
@@ -12,7 +12,7 @@ const style = {
   left: '50%',
   transform: 'translate(-50%, -50%)',
   width: 400,
-  bgcolor: 'background.paper',
+  bgcolor: 'black',
   border: '2px solid #000',
   boxShadow: 24,
   p: 4,
@@ -33,6 +33,9 @@ export default function BasicModal() {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Tags
+          </Typography>
           <TagList/>
         </Box>
       </Modal>
--- src/app/taglist.tsx
+++ src/app/taglist.tsx
@@ -1,3 +1,19 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '10%',
+    left: '10%',
+    transform: 'translate(-50%, -50%)',
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 
 export async function TagList() {
     const response = await fetch('https://api.mangadex.org/manga/tag');
@@ -5,15 +21,11 @@ export async function TagList() {
       const tagResponse = await response.json();
   
       const tagList = tagResponse.data.map((tag: { attributes: { name: { en: any; }; }; }) => {
-          return tag.attributes.name.en;
+          return(
+            <><Button key={tag.attributes.name.en}> {tag.attributes.name.en} </Button></>
+          );
       })
-
-      const boxContent = 
-            <div>
-                <h4>Tags</h4>
-                <button>{tagList}</button>
-            </div>
   
-    return boxContent
+    return tagList
     
   }
