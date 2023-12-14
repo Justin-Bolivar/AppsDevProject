83696762c519c4bbbf943a6d7584299cec3b6a32
Justin Bolivar <justinantonio.bolivar.21@usjr.edu.ph>
2023-12-14 13:15:57 +0800

    added centering to searchbar and like

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,17 @@ import Image from 'next/image'
 import SearchBar from './components/SearchBar'
 import LikeButton from './components/LikeButton'
 import Link from 'next/link'
+import { Container } from 'postcss'
 
 export default function Home() {
   return (
     <main>
+      <div className="page-container">
       <SearchBar />
       <Link href="/LikedManga">
       <LikeButton />
       </Link>
+      </div>
     </main>
   )
 }
