8582f54f094cbd36cdda14f057773b5c65ff675d
Ethan <ethan.ortiz.21@usjr.edu.ph>
2023-12-14 15:06:35 +0800

    MangaCard fixed

--- src/app/components/MangaCard.tsx
+++ src/app/components/MangaCard.tsx
@@ -11,22 +11,30 @@ interface Props {
 }
 
 interface Manga {
-  data: {
-    tag: {
+    tags: {
       attributes: {
         name: {
           en: string;
         };
       };
     }[];
-  };
+  
+}
+
+interface Tag {
+    attributes: {
+        name: {
+          en: string;
+        };
+      };
 }
 
 async function MangaCard({ title }: Props) {
   const url = "https://api.mangadex.org/manga/";
-  const response = await fetch(`${url}?title=${title}`, {method: "GET"});
+  const response = await fetch(`${url}?title=${title}`);
   if (!response.ok) throw new Error("Failed to fetch user");
   const manga = await response.json();
+  console.log(manga);
 
   const picture =
     "https://mangadex.org/covers/d65c0332-3764-4c89-84bd-b1a4e7278ad7/3fa0e8cc-0e39-4cb8-8f25-d70fb86cf110.jpg";
@@ -37,14 +45,14 @@ async function MangaCard({ title }: Props) {
       <CardMedia component="img" height="140" image={picture} alt={synopsis} />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          {title}
+          {manga.data[0].attributes.title.en}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          {synopsis}
+          {manga.data[0].attributes.description.en}
         </Typography>
         <div>
-          {manga.data.tag.map((tag) => (
-            <Chip key={tag.attributes.name.en} label={tag.attributes.name.en} />
+          {manga.data[0].attributes.tags.map((tag : any) => (
+            <Chip key={tag.attributes.name.en} label={tag.attributes.name.en} /> 
           ))}
         </div>
       </CardContent>
