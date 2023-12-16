0884ef8436635af6bec0702bc5db285030e198eb
Donnalyn <donnalynrrma@gmail.com>
2023-12-16 08:20:30 +0800

    updated layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import SearchBar from './components/searchBar'
+import { Box } from '@mui/material'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,9 +15,14 @@ export default function RootLayout({children,}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <body className='bg-gradient-to-r from-fuchsia-950 via-violet-300 to-fuchsia-950'>
-        <div style={{ position: 'fixed', top: 0, width: '100%', zIndex: 1000 }}>
-          <SearchBar/>
+        <div style={{ position: 'fixed', top: 0, width: '100%', zIndex: 1000}}>
+          <center>
+            <SearchBar/>
+          </center>
         </div>
+        <br></br>
+        <br></br>
+        <br></br>
         {children }
         </body>
     </html>
